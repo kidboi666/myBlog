b3aa3b65404757a1d1a774e32d997bf1cb777d36
kidboi666 <kidboi666@gmail.com>
2024-08-11 16:07:44 +0900

    chore : 기본적인 린트설정 완성, feat : Button 컴포넌트 구현

--- components/common/Button/Button.tsx
+++ components/common/Button/Button.tsx
@@ -1,8 +1,24 @@
-import { PropsWithChildren } from "react"
+import cn from "@/lib/cn"
+import { cva } from "class-variance-authority"
+import { ComponentProps, PropsWithChildren } from "react"
 
-export default function Button({ children }: PropsWithChildren) {
+interface Props extends ComponentProps<"button"> {
+  variant?: "primary" | "secondary" | "teritory"
+}
+
+const buttonVariants = cva("rounded-full px-6 py-1 text-white", {
+  variants: {
+    variant: {
+      primary: "bg-slate-800 hover:bg-slate-300 hover:text-slate-800",
+      secondary: "",
+      teritory: "",
+    },
+  },
+})
+
+export const Button = ({ children, onClick, variant = "primary" }: PropsWithChildren<Props>) => {
   return (
-    <button type="button" onClick={() => console.log("asdf")}>
+    <button type="button" onClick={onClick} className={cn(buttonVariants({ variant }))}>
       {children}
     </button>
   )

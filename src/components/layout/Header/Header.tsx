f7615aa7056f19278e59798ddee6283c3ecfea52
kidboi666 <kidboi666@gmail.com>
2024-08-18 13:28:31 +0900

    feat : Nav에 드롭다운 리스트 로직 수정

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,12 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import Link from "next/link"
-
 import { useRouter } from "next/router"
+import { MouseEvent } from "react"
 import { useQuery } from "@tanstack/react-query"
 
 import { useStatusChange } from "@/src/hooks/useStatusChange"
 import { meQuery } from "@/src/services/queries/auth/meQuery"
+import { categoryQuery } from "@/src/services/queries/category/categoryQuery"
 
-import { ArticleMenu } from "../../feature/nav/ArticleMenu"
 import { Button } from "../../shared/Button"
 import { MeIcon } from "../../icon/MeIcon"
 import { MenuIcon } from "../../icon/MenuIcon"
@@ -14,27 +15,40 @@ import { SearchIcon } from "../../icon/SearchIcon"
 import { Container } from "../Container"
 import { List } from "../List/List"
 import { SearchBar } from "../../feature/nav/SearchBar"
+import { DropDownList } from "../../shared/DropDown/DropDowList"
+
+const meMenu = [{ name: "로그아웃", id: 0 }]
 
 export const Header = () => {
   const [menuRef, menuStatusRef, handleMenuStatusChange] = useStatusChange<
     HTMLButtonElement,
-    HTMLDivElement
+    HTMLUListElement
   >()
   const [searchRef, searchStatusRef, handleSearchStatusChange] = useStatusChange<
     HTMLButtonElement,
     HTMLDivElement
   >()
+  const [meBtnRef, meBtnStatusRef, handleMeBtnStatusChange] = useStatusChange<
+    HTMLButtonElement,
+    HTMLUListElement
+  >()
   const router = useRouter()
+  const { data: categories } = useQuery(categoryQuery.queryOptions())
   const { data: me } = useQuery(meQuery.queryOptions())
 
-  const handleAuthButton = () => {
-    console.log(me?.user.user_metadata)
-    if (me?.user.user_metadata) {
-      // 뭔가를 할거임
+  const handleAuthButton = (e: MouseEvent<HTMLButtonElement>) => {
+    if (me?.user) {
+      handleMeBtnStatusChange(e)
     } else {
       router.push("/auth/signin")
     }
   }
+
+  const handleMenuClick = (menu: Record<string, any>) => {
+    router.push(`blog/${menu.id}`)
+  }
+
+  const handleMeBtnClick = () => {}
 
   return (
     <Container
@@ -57,16 +71,27 @@ export const Header = () => {
               <SearchBar statusRef={searchStatusRef} />
             </Button>
           </List.Row>
-          <List.Row>
+          <List.Row className="relative">
             <Button variant="icon" ref={menuRef} onClick={handleMenuStatusChange}>
               <MenuIcon />
             </Button>
-            <ArticleMenu statusRef={menuStatusRef} />
+            <DropDownList
+              ref={menuStatusRef}
+              itemList={categories}
+              onClick={handleMenuClick}
+              className="right-0 w-40"
+            />
           </List.Row>
-          <List.Row>
-            <Button variant="icon" onClick={handleAuthButton}>
+          <List.Row className="relative">
+            <Button variant="icon" ref={meBtnRef} onClick={handleAuthButton}>
               <MeIcon />
             </Button>
+            <DropDownList
+              ref={meBtnStatusRef}
+              itemList={meMenu}
+              onClick={handleMeBtnClick}
+              className="right-0 w-40"
+            />
           </List.Row>
         </List>
       </nav>
